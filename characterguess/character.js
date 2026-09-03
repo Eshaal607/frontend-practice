8f0bcf845a0977d583440a4eb4820e414363b1d2
Eshaal607 <eshaalbaloch141@gmail.com>
2026-09-03 23:08:01 +0500

    started making charactergame

---
+++ characterguess/character.js
@@ -0,0 +1,18 @@
+let questions = [
+    "You discover a mysterious door in a place you've never been before. What do you do?",
+    "Your closest friend has made a terrible mistake and refuses to admit it. What do you do?",
+    "You suddenly have the opportunity to become extremely powerful. What's your first thought?",
+]
+
+let startEl = document.getElementById("start-btn")
+let objEl = document.getElementById("obj")
+
+startEl.addEventListener('click', startgame)
+
+function startgame(){
+   objEl.innerHTML = ""
+}
+
+function generatequestion{
+    
+}
